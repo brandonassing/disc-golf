19653c44d8bea6860f25696394c47327ac5b7561
Brandodn Assing <assing.brandon@gmail.com>
2022-06-09 22:42:47 -0400

    Added dynamic default hole name and par

--- disc-golf/Features/Round/RoundViewModel.swift
+++ disc-golf/Features/Round/RoundViewModel.swift
@@ -22,8 +22,26 @@ class RoundViewModel: ObservableObject {
 	
 	init(scorecard: Scorecard?) {
 		
-		let parSubject = CurrentValueSubject<Int, Never>(RoundViewModel.defaultPar.rawValue)
-		let holeNameSubject = CurrentValueSubject<String, Never>(RoundViewModel.defaultHoleName)
+		var defaultHole = Hole(name: RoundViewModel.defaultHoleName, par: RoundViewModel.defaultPar.rawValue, strokes: nil)
+		if let scorecard = scorecard, !scorecard.holes.isEmpty {
+			self.scorecard = scorecard
+			if let firstHole = scorecard.holes.first {
+				defaultHole = firstHole
+			}
+		} else {
+			self.scorecard = Scorecard(name: nil, holes: [defaultHole])
+//			self.scorecard = Scorecard(name: nil, holes: [
+//				Hole(name: "1", par: 3, strokes: 3), Hole(name: "2", par: 4, strokes: 3), Hole(name: "3", par: 3, strokes: 4),
+//				Hole(name: "4", par: 3, strokes: 3), Hole(name: "5", par: 3, strokes: 3), Hole(name: "6L", par: 3, strokes: 5),
+//				Hole(name: "7", par: 5, strokes: 4), Hole(name: "8", par: 4, strokes: 4), Hole(name: "9", par: 4, strokes: 4),
+//				Hole(name: "10", par: 3, strokes: 3), Hole(name: "11", par: 4, strokes: 5), Hole(name: "12", par: 4, strokes: nil),
+//			])
+		}
+
+		self.currentHole = defaultHole
+		
+		let parSubject = CurrentValueSubject<Int, Never>(defaultHole.par)
+		let holeNameSubject = CurrentValueSubject<String, Never>(defaultHole.name)
 		let previousHoleSubject = PassthroughSubject<Void, Never>()
 		let nextHoleSubject = PassthroughSubject<Void, Never>()
 		self.inputs = Inputs(
@@ -32,21 +50,12 @@ class RoundViewModel: ObservableObject {
 			previousHole: previousHoleSubject,
 			nextHole: nextHoleSubject
 		)
-		
-		let defaultHole = Hole(name: RoundViewModel.defaultHoleName, par: RoundViewModel.defaultPar.rawValue, strokes: nil)
-		if let scorecard = scorecard, !scorecard.holes.isEmpty {
-			self.scorecard = scorecard
-			self.currentHole = scorecard.holes.first ?? defaultHole
-		} else {
-			self.scorecard = Scorecard(name: nil, holes: [defaultHole])
-			self.currentHole = defaultHole
-//			self.scorecard = Scorecard(name: nil, holes: [
-//				Hole(name: "1", par: 3, strokes: 3), Hole(name: "2", par: 4, strokes: 3), Hole(name: "3", par: 3, strokes: 4),
-//				Hole(name: "4", par: 3, strokes: 3), Hole(name: "5", par: 3, strokes: 3), Hole(name: "6L", par: 3, strokes: 5),
-//				Hole(name: "7", par: 5, strokes: 4), Hole(name: "8", par: 4, strokes: 4), Hole(name: "9", par: 4, strokes: 4),
-//				Hole(name: "10", par: 3, strokes: 3), Hole(name: "11", par: 4, strokes: 5), Hole(name: "12", par: 4, strokes: nil),
-//			])
-		}
+				
+		Publishers.CombineLatest(parSubject, holeNameSubject)
+			.map({ par, holeName in
+				Hole(name: holeName, par: par, strokes: nil)
+			})
+			.assign(to: &self.$currentHole)
 		
 	}
 	
