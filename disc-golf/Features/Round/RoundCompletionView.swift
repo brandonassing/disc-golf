367a590eee8de916cc98760ff78710fc45cae253
Brandodn Assing <assing.brandon@gmail.com>
2022-06-20 07:36:30 -0700

    Changed nav to use modals for easier dismissal

--- disc-golf/Features/Round/RoundCompletionView.swift
+++ disc-golf/Features/Round/RoundCompletionView.swift
@@ -2,15 +2,16 @@
 import SwiftUI
 
 struct RoundCompletionView: View {
-	
+
 	let scorecard: Scorecard
-	
+	let dismiss: () -> ()
+		
     var body: some View {
 		VStack(spacing: 50) {
 			ScorecardView(viewModel: ScorecardViewModel(scorecard: self.scorecard))
 				.padding()
 
-			Button(action: {}) {
+			Button(action: { self.dismiss() }) {
 				Text("Finish")
 			}
 			.buttonStyle(.bordered)
--- disc-golf/Features/Round/RoundView.swift
+++ disc-golf/Features/Round/RoundView.swift
@@ -6,88 +6,98 @@ struct RoundView: View {
 	@State private var holeName = RoundViewModel.defaultHoleName
 	
 	@StateObject private var viewModel: RoundViewModel
+	
+	@Environment(\.dismiss) var dismiss
 
 	init(scorecard: Scorecard? = nil) {
 		self._viewModel = StateObject(wrappedValue: RoundViewModel(scorecard: scorecard))
 	}
 	
     var body: some View {
-		VStack(spacing: 50) {
-			ScorecardView(viewModel: ScorecardViewModel(scorecard: self.viewModel.scorecard))
-				.padding()
-			
-			VStack {
+		NavigationView {
+			VStack(spacing: 50) {
+				ScorecardView(viewModel: ScorecardViewModel(scorecard: self.viewModel.scorecard))
+					.padding()
+				
 				VStack {
-					Text("Par")
+					VStack {
+						Text("Par")
+						
+						Picker("Par", selection: self.$viewModel.parOption) {
+							ForEach(RoundViewModel.ParOption.allCases, id: \.rawValue) { parOption in
+								Text("\(parOption.rawValue)")
+									.tag(parOption)
+							}
+						}
+						.pickerStyle(.segmented)
+					}
 					
-					Picker("Par", selection: self.$viewModel.parOption) {
-						ForEach(RoundViewModel.ParOption.allCases, id: \.rawValue) { parOption in
-							Text("\(parOption.rawValue)")
-								.tag(parOption)
+					Spacer()
+						.frame(height: 20)
+					
+					VStack(spacing: 10) {
+						Text("Throws")
+						
+						HStack(spacing: 50) {
+							Button(action: self.viewModel.inputs.decreaseStrokes.send) {
+								Image(systemName: "minus")
+							}
+							
+							if let strokes = self.viewModel.strokes {
+								Text("\(strokes)")
+							} else {
+								Text("-")
+							}
+							
+							Button(action: self.viewModel.inputs.increaseStrokes.send) {
+								Image(systemName: "plus")
+							}
 						}
 					}
-					.pickerStyle(.segmented)
-				}
-				
-				Spacer()
-					.frame(height: 20)
-				
-				VStack(spacing: 10) {
-					Text("Throws")
+					
+					Spacer()
 					
 					HStack(spacing: 50) {
-						Button(action: self.viewModel.inputs.decreaseStrokes.send) {
-							Image(systemName: "minus")
+						Button(action: self.viewModel.inputs.previousHole.send) {
+							Image(systemName: "chevron.left")
 						}
 						
-						if let strokes = self.viewModel.strokes {
-							Text("\(strokes)")
-						} else {
-							Text("-")
-						}
+						// TODO: prevent empty string. Maybe just disable next/back button? This would allow user to completely change a hole name (ie: change "1" to "2")
+						TextField("Hole", text: self.$holeName)
+							.frame(width: 50)
+							.multilineTextAlignment(.center)
+							.onChange(of: self.holeName, perform: { holeName in
+								// TODO: should this logic be moved to vm?
+								guard holeName.count <= 2 else {
+									self.holeName = String(holeName.prefix(2))
+									return
+								}
+								self.viewModel.inputs.holeName.send(holeName)
+							})
+							.onReceive(self.viewModel.$currentHole, perform: { currentHole in
+								self.holeName = currentHole.name
+							})
 						
-						Button(action: self.viewModel.inputs.increaseStrokes.send) {
-							Image(systemName: "plus")
+						Button(action: self.viewModel.inputs.nextHole.send) {
+							Image(systemName: "chevron.right")
 						}
 					}
 				}
 				
-				Spacer()
-				
-				HStack(spacing: 50) {
-					Button(action: self.viewModel.inputs.previousHole.send) {
-						Image(systemName: "chevron.left")
-					}
-					
-					// TODO: prevent empty string. Maybe just disable next/back button? This would allow user to completely change a hole name (ie: change "1" to "2")
-					TextField("Hole", text: self.$holeName)
-						.frame(width: 50)
-						.multilineTextAlignment(.center)
-						.onChange(of: self.holeName, perform: { holeName in
-							// TODO: should this logic be moved to vm?
-							guard holeName.count <= 2 else {
-								self.holeName = String(holeName.prefix(2))
-								return
-							}
-							self.viewModel.inputs.holeName.send(holeName)
-						})
-						.onReceive(self.viewModel.$currentHole, perform: { currentHole in
-							self.holeName = currentHole.name
-						})
-					
-					Button(action: self.viewModel.inputs.nextHole.send) {
-						Image(systemName: "chevron.right")
+				Button(action: {}) {
+					NavigationLink(
+						destination: RoundCompletionView(
+							scorecard: self.viewModel.scorecard,
+							dismiss: { self.dismiss.callAsFunction() }
+						)
+					) {
+						Text("End round")
 					}
 				}
+				.buttonStyle(.bordered)
 			}
-			
-			Button(action: {}) {
-				NavigationLink(destination: RoundCompletionView(scorecard: self.viewModel.scorecard)) {
-					Text("End round")
-				}
-			}
-			.buttonStyle(.bordered)
+			.padding()
+		
 		}
-		.padding()
 	}
 }
