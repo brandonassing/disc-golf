0dab34681036c52c33c9e54cd562ab538a32770d
Brandodn Assing <assing.brandon@gmail.com>
2022-06-23 08:25:23 -0700

    Added course name view

--- disc-golf/Features/Round/RoundCompletionView.swift
+++ disc-golf/Features/Round/RoundCompletionView.swift
@@ -4,7 +4,9 @@ import SwiftUI
 struct RoundCompletionView: View {
 
 	@StateObject private var viewModel: RoundCompletionViewModel
+	@State private var isActive = false
 	private let dismiss: () -> ()
+	
 
 	init(scorecard: Scorecard, dismiss: @escaping () -> ()) {
 		self._viewModel = StateObject(wrappedValue: RoundCompletionViewModel(scorecard: scorecard))
@@ -16,11 +18,22 @@ struct RoundCompletionView: View {
 			ScorecardView(viewModel: ScorecardViewModel(scorecard: self.viewModel.scorecard))
 				.padding()
 
-			Button(action: { self.dismiss() }) {
+			Button(action: {
+				if !self.viewModel.needsName {
+					self.dismiss()
+					return
+				}
+				self.isActive = self.viewModel.needsName
+			}) {
 				Text("Finish")
 			}
 			.buttonStyle(.bordered)
-			
+			.sheet(isPresented: self.$isActive) {
+				RoundNameView { name in
+					self.viewModel.courseName = name
+				}
+			}
+
 			Spacer()
 		}
     }
--- disc-golf/Features/Round/RoundCompletionViewModel.swift
+++ disc-golf/Features/Round/RoundCompletionViewModel.swift
@@ -5,6 +5,8 @@ import Foundation
 class RoundCompletionViewModel: ObservableObject {
 	
 	@Published var scorecard: Scorecard
+	@Published var needsName: Bool = true
+	@Published var courseName: String = ""
 	
 	init(scorecard: Scorecard) {
 		
@@ -15,5 +17,14 @@ class RoundCompletionViewModel: ObservableObject {
 			startTime: scorecard.startTime,
 			endTime: Date()
 		)
+		
+		self.$scorecard
+			.map({ $0.name == nil })
+			.assign(to: &self.$needsName)
+		
+		self.$courseName
+			.filter({ !$0.isEmpty })
+			.map({ Scorecard(id: self.scorecard.id, name: $0, holes: self.scorecard.holes, startTime: self.scorecard.startTime, endTime: self.scorecard.endTime) })
+			.assign(to: &self.$scorecard)
 	}
 }
